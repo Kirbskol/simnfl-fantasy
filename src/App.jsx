de6e9fe48d2f2aabab87d862030e29af65030ba8
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-08 14:24:41 +0000

    Create initial loading screen with input for name and button
    Initial loading screen in App.jsx starting to be configured, asking
    user to enter username and click button to start draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div>
-      <h1>SimNFL Fantasy</h1>
+    <div class="flex-col p-8 items-center text-center">
+      <div class="text-center w-1/2 justify-self-center">
+        <h1 class="text-4xl self-center mb-2">Welcome to SimNFL Fantasy Draft</h1>
+        <p class="text-lg text-wrap">At present we are only offering a Fantasy Draft option, where you can export your drafted team as an image and show it off to other users.</p>
+      </div>
+      <div class="text-center">
+        <input class="bg-slate-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" type="text" placeholder="Username"></input>
+        <button class="bg-slate-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enter Draft</button>
+      </div>
     </div>
   )
 }

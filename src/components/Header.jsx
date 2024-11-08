c72e6ca5b282ec3b52c009f78c165a5b0ab20e53
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-08 20:44:56 +0000

    Add href to logo to redirect back to home page
    <a> put around logo in Header.jsx to link back to '/'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,11 +29,11 @@ export default function Header() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <img
+              <a href="/"><img
                 alt="Your Company"
                 src={logo}
                 className="h-12 w-auto"
-              />
+              /></a>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">

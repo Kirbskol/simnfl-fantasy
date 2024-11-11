9d7ad9046b026268dc439ed2737b833ebe889d10
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-11 15:48:08 +0000

    Begin creating fetchPlayers function in Draft.jsx
    This function will use an api to pull data from the .json files I have
    created for the player cards. I have created the initial line, which
    will use async for the api. The first section will check if isFetching
    is running, and if it is, it will change isFetching to true and exit.

--- src/components/Draft.jsx
+++ src/components/Draft.jsx
@@ -8,12 +8,17 @@ const Draft = () => {
     const [wrCount, setWrCount] = useState(0)
     const [olCount, setOlCount] = useState(0)
     const isFetching = useRef(false)
-    
+
     const manualPositionIndex = useRef(0)
     const manualPositionQueue = [
         "qb", "rb", "te", "wr", "wr", "wr", "ol",
         "ol", "ol", "ol", "ol", "k", "def"
     ]
+
+    const fetchPlayers = async () => {
+        if (isFetching.current) return
+            isFetching.current = true
+    }
 
 }
 

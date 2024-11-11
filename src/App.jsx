5dd6b4deab5c2f5a03ba8e342d9770b6702b5273
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-11 15:40:51 +0000

    Tidied presentation of code
    Re worked slightly to improve presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home.jsx';
-import Draft from './components/Draft.jsx';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Draft from './components/Draft.jsx'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path="/draft" element={<Draft />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
 export default App;

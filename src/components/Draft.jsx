b548a2161c5a63fe16c02f7a3e37d429993ce97e
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-11 15:30:51 +0000

    Set up state management for Draft.jsx
    Imported useState, useEffect and useRef from React
    Variety of state variables set using useState
    Also defined two references using useRef.

--- src/components/Draft.jsx
+++ src/components/Draft.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-const Draft = ({ userName }) => {
-  const [players, setPlayers] = useState([])
-  const [selectedPlayer, setSelectedPlayer] = useState(null)
-  const [position, setPosition] = useState('qb')
-  const [selectedPlayers, setSelectedPlayers] = useState([])
-  const [wrCount, setWrCount] = useState(0)
-  const [olCount, setOlCount] = useState(0)
-  const isFetching = useRef(false)
+const Draft = () => {
+    const [players, setPlayers] = useState([])
+    const [selectedPlayer, setSelectedPlayer] = useState(null)
+    const [position, setPosition] = useState('qb')
+    const [selectedPlayers, setSelectedPlayers] = useState([])
+    const [wrCount, setWrCount] = useState(0)
+    const [olCount, setOlCount] = useState(0)
+    const isFetching = useRef(false)
+    const manualPositionIndex = useRef(0)
+}
 
-  const manualPositionQueue = [
-    'qb', 'rb', 'te', 'wr', 'wr', 'wr', 
-    'ol', 'ol', 'ol', 'ol', 'ol', 'k', 'def'
-  ]
-  const manualPositionIndex = useRef(0)
+export default Draft

5d393040517d52c1bd8b716880412806d3f9135e
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-12 16:34:28 +0000

    Put cardClass from PlayerCard into an array to make it more readable
    The styling for cardClass has been put into an array as it was very
    long and taking up a huge amount of space on the screen in one long
    line, this is now more readable.

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -3,8 +3,22 @@ import React from 'react'
 const PlayerCard = (props) => {
     const { ID, POS, FIRSTNAME, LASTNAME, EXP, ARCH, OVR, LOGO, ICON } = props
 
+    const cardClass = [
+        'player-card', 
+        'p-4', 
+        'border-2', 
+        'border-gray-300', 
+        'rounded-lg', 
+        'shadow-md', 
+        'flex', 
+        'flex-col', 
+        'items-center', 
+        OVR >= 80 ? 'bg-shiny-gold text-white' : 'bg-shiny-dark-blue text-white', 
+        'flex-grow'
+    ].join(' ')
+
     return (
-        <div className={`player-card p-4 border-2 border-gray-300 rounded-lg shadow-md flex flex-col items-center ${OVR >= 80 ? 'bg-shiny-gold text-white' : 'bg-shiny-dark-blue text-white'} flex-grow`} style={{ width: 'calc(33.333% - 10px)', margin: '5px' }} onClick={() => handlePlayerSelect(ID)}>
+        <div className={cardClass} style={{ width: 'calc(33.333% - 10px)', margin: '5px' }} onClick={() => handlePlayerSelect(ID)}>
             <div className="text-3xl font-bold mb-2 p-4 w-24 h-24 rounded-full bg-[#0f172a] text-white text-outline-black flex flex-col items-center justify-center">
                 {OVR}
                 <span className="text-xs">OVR</span>

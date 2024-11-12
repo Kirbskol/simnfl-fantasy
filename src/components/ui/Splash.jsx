e474a5a4d5c700f54e9e72f5a66161c97606568e
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-12 16:45:37 +0000

    Change Splash.jsx text
    Text changed on Splash.jsx, this is displayed when the web app
    immediately opens.

--- src/components/ui/Splash.jsx
+++ src/components/ui/Splash.jsx
@@ -4,13 +4,12 @@ const Splash = () => {
   return (
     <div class="flex-col p-8 items-center text-center">
         <div class="text-center w-1/2 justify-self-center">
-            <h1 class="text-4xl self-center mb-2">Welcome to SimNFL Fantasy Draft</h1>
-            <p class="text-lg text-wrap">At present we are only offering a Fantasy Draft option, 
-                                         where you can export your drafted team as an image and 
-                                         show it off to other users.</p>
+            <h1 class="text-4xl font-bold self-center mb-2">Welcome to SimNFL Fantasy Draft</h1>
+            <p class="text-xl text-wrap mb-2">Enter your username and start drafting. Good luck!</p>
+            <p class="text-sm italic text-wrap">Disclaimer: If you don't know who Matt Howard is then you're in the wrong place.</p>
         </div>
     </div>
   )
 }
 
-export default Splash;
+export default Splash

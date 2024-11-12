64a61454f37e817fc8934e4acb1d2307c39bf668
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-12 14:35:45 +0000

    Changed player json files
    Added ICON section to all player position json files as some were not
    there

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const PlayerCard = (props) => {
-
+    const position = {props.POS}
+    const firstName = {props.FIRSTNAME}
+    const lastName = {props.LASTNAME}
+    const overall = {props.OVR}
 }
 
 export default PlayerCard

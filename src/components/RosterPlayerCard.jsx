cb5d2f5f82e8122c023ea6522b4a55df9779842d
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-13 16:09:41 +0000

    Create RosterPlayerCard.jsx
    This will be a variation of PlayerCard.jsx, with inherited properties
    and amended ones to cater for the Roster page layout. To be completed.

---
+++ src/components/RosterPlayerCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react' 
+import PlayerCard from './PlayerCard'
+
+const RosterPlayerCard = (props) => {
+    const { ID, POS, FIRSTNAME, LASTNAME, EXP, ARCH, OVR, LOGO, ICON } = props
+
+    return(
+        <h1>to be completed</h1>
+    )
+}
+
+export default RosterPlayerCard

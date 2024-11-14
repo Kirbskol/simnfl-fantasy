0f59b19ae58ac77aec00d33e7768bbff00f1266a
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-14 13:39:29 +0000

    Construct HC card, and sort all cards into sorted array to match grid
    Cards are now, after being selected, sorted into array which
    corresponds with the grid locations, including a HC card with an average
    rating of all cards added together.

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
-import RosterPlayerCard from './PlayerCard'
+import RosterPlayerCard from './RosterPlayerCard.jsx'
 
 const Roster = () => {
     const location = useLocation()
     const { selectedPlayers, userName } = location.state || { selectedPlayers: [], userName: '' }
+    const orderedPlayers = []
+    const positionsOrder = ["OL", "TE", "HC", "WR", "DEF", "RB", "QB", "K"]
+    const totalOVR = selectedPlayers.reduce((sum, player) => sum + player.OVR, 0)
+    const avgOVR = Math.round(totalOVR / selectedPlayers.length)
+    const hcDetails = {
+        ID: 'HC',
+        TEAM: '',
+        FIRSTNAME: userName,
+        LASTNAME: '',
+        POS: 'HC',
+        OVR: avgOVR,
+        LOGO: '',
+        ICON: ''
+    }
+    const finalCards = [hcDetails, ...selectedPlayers]
+    positionsOrder.forEach(pos => {
+        finalCards.forEach(player => {
+            if (player.POS === pos) {
+                orderedPlayers.push(player)
+            }
+        })
+    })
 
     return(
         <div className="grid grid-cols-8 grid-rows-3 gap-3">

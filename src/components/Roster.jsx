fe98a537f8ea7415e0aef7d419be50ec272f5e30
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-14 16:13:34 +0000

    Add export button to Roster.jsx and adjust margins
    Export button added to roster page but not functional as yet, styled
    correctly.
    Margins adjusted so coach card sits in line with export button

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -19,8 +19,9 @@ const Roster = () => {
     })
 
     return (
-        <div className="p-8 text-center">
-            <div className="flex items-center justify-center">
+        <div className="p-4 text-right">
+            <button class="border-2 border-gray-300 bg-shiny-dark-blue hover:bg-shiny-gold hover:text-blue-950 text-white text-sm font-bold mt-0 py-2 px-4 rounded-xl">Export</button>
+            <div className="flex items-center -mt-9 justify-center">
                 <div className={`text-2xl font-bold border-2 border-gray-300 mb-2 p-3 w-32 h-26 rounded-xl ${avgOVR >= 80 ? 'bg-shiny-gold text-white' : 'bg-shiny-dark-blue text-white'} text-outline-black flex items-center justify-center`}>
                     <div className="text-2xl font-bold mb-2 p-3 w-16 h-16 text-white text-outline-black flex flex-col items-center justify-center">
                         HC

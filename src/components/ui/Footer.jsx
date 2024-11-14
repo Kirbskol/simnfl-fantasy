31908a2596bd7bd4ee9b45d9ed305337b9d8a491
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-14 19:08:29 +0000

    Footer.jsx created and deployed on all pages
    Links to Kirbskol github page

---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer class="bg-white mt-auto md:mt-auto shadow bottom-0 dark:bg-slate-900">
+            <div class="w-full mx-auto max-w-screen-xl flex justify-center p-4 md:flex md:items-center md:justify-center">
+                <span class="text-xs text-center text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://github.com/Kirbskol/" class="hover:underline">Kirbskol</a>. All Rights Reserved.</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Navbar from './components/ui/Navbar.jsx'
+import Footer from './components/ui/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </StrictMode>,
 )

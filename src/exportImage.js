1a614ac9619347dfb6a870ab6fa00f0eed105c5b
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-14 15:51:11 +0000

    Install html2canvas and create exportImage.js
    This has not yet been implemented into Draft.jsx, but followed
    documentation and installed successfully. Also followed Tailwind
    direction and installed an addition to the index.css to cater for
    potential distortion of text when exporting image

---
+++ src/exportImage.js
@@ -0,0 +1,6 @@
+import html2canvas from "html2canvas"
+
+const exportImage = async (el, imageFileName) => {
+    const canvas = await html2canvas(element)
+    const image = canvas.toDataURL("image/png", 1.0)
+}

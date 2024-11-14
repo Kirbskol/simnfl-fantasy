557fa33cb32821d1df59aab0a13d5287679089b9
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-11-14 12:02:54 +0000

    Change grid structure and add position placeholders

--- src/components/Roster.jsx
+++ src/components/Roster.jsx
@@ -3,24 +3,24 @@ import { useLocation } from 'react-router-dom'
 import PlayerCard from './PlayerCard'
 
 const Roster = () => {
-    
+
 
     return(
-        <div className="grid grid-cols-7 grid-rows-3 gap-3">
-            <div >1</div>
-            <div >2</div>
-            <div >3</div>
-            <div >4</div>
-            <div >5</div>
-            <div className="col-start-7">6</div>
-            <div className="row-start-2">7</div>
-            <div className="row-start-2">8</div>
-            <div className="col-start-4 row-start-2">9</div>
-            <div className="col-start-5 row-start-2">10</div>
-            <div className="col-start-7 row-start-2">11</div>
-            <div className="col-start-2 row-start-3">12</div>
-            <div className="col-start-3 row-start-3">13</div>
-            <div className="col-start-7 row-start-3">14</div>
+        <div className="grid grid-cols-8 grid-rows-3 gap-3">
+            <div >OL</div>
+            <div >OL</div>
+            <div >OL</div>
+            <div >OL</div>
+            <div >OL</div>
+            <div >TE</div>
+            <div className="col-start-8">HC</div>
+            <div className="row-start-2">WR</div>
+            <div className="row-start-2">WR</div>
+            <div className="col-start-6 row-start-2">WR</div>
+            <div className="col-start-8 row-start-2">DEF</div>
+            <div className="col-start-3 row-start-3">RB</div>
+            <div className="col-start-4 row-start-3">QB</div>
+            <div className="col-start-8 row-start-3">K</div>
         </div>
     )
 }
